8c6e70bc389af62e3c670b3eb7c7dd81197d67db
Vladimir Kavlakan <vladimirkavlakan@vladimirs-mbp.trendtech.ru>
2017-06-06 12:28:06 +0700

    Initial Commit

---
+++ Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by Vladimir Kavlakan on 06/06/2017.
+//  Copyright © 2017 Vladimir Kavlakan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
